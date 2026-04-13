09e6c66fac2a0e13d85e9c22d0e3510e256ec8be
Jaime Rodriguez <rpjosejaime@gmail.com>
2026-04-13 17:53:46 -0600

    feat, cambiando hola mundo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app =  Flask(__name__)
 #Ruta raiz
 @app.route('/')
 def index():
-    return 'Hola Mundo, actualizado'
+    return 'Hola Mundo, actualizado 13 ABRIL'
 
 if __name__ == '__main__':
     app.run(debug=True)
